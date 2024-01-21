de54b9c8c74195d54c7535fee3ef6976466c6299
JONGGON <rlawhdrhs27@gmail.com>
2024-01-21 17:15:07 +0900

    update

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 import type { SiteConfig } from '@/types';
 
 export const siteConfig: SiteConfig = {
-    name: 'Deepfocused Next.js Resume',
-    description: 'A page explaining Deepfocused career and skills.',
+    name: 'Deepfocused Resume',
+    description: 'A page explaining Deepfocused career and skills',
     url: 'https://deepfocused-resume.vercel.app/',
     ogImageUrl: '/images/we.jpg',
     links: {
