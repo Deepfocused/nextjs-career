36491143915963a23debb5ded6a1846b10b218d0
JONGGON <rlawhdrhs27@gmail.com>
2024-01-22 00:56:56 +0900

    update

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -30,25 +30,21 @@ export const siteMetadata = {
         icon: '/favicon.png',
         other: [
             {
-              url: 'splashscreens/iphone5_splash.png',
-              media:
-                "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
-              rel: 'apple-touch-startup-image',
+                url: 'splashscreens/iphone5_splash.png',
+                media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+                rel: 'apple-touch-startup-image',
             },
             {
                 url: 'splashscreens/iphoneplus_splash.png',
-                media:
-                  "(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)",
+                media: '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)',
                 rel: 'apple-touch-startup-image',
-              },
+            },
             {
                 url: 'splashscreens/ipad_splash.png',
-                media:
-                  "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)",
+                media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)',
                 rel: 'apple-touch-startup-image',
-            }, 
-          ],
+            },
+        ],
     },
     manifest: '/manifest.webmanifest',
 };
-
