7292247f764212bff1f140a8a1a47141ef00bcaa
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2024-01-22 18:10:57 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,11 +59,8 @@ function Home() {
         <motion.div
             className="mx-auto mb-[7%] mt-[7%] h-full w-[98%] w-full bg-white min-[640px]:w-3/5 min-[1024px]:w-2/5"
             initial={{ opacity: 0, scale: 0.0, y: '0%' }}
-            animate={{
-                opacity: [0.0, 0.1, 0.2, 0.4, 0.8, 1],
-                scale: [1, 1, 1, 1, 1, 1], // x : ["5%", "0%"], y: ["0%", "5%"]
-            }}
-            transition={{ type: "spring", duration: 2 }}
+            animate={{ rotate: 180 }}
+            transition={{ type: 'spring', velocity: 2 }}
         >
             <div className="px-8 py-8">
                 <section className="flex items-center">
