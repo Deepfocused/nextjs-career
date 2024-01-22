4796b146d32d9b333e9856021d358920f43d66ad
JONGGON <rlawhdrhs27@gmail.com>
2024-01-23 02:05:18 +0900

    update

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -28,6 +28,59 @@ export const siteMetadata = {
     },
     icons: {
         icon: '/favicon.png',
-    },
+        apple: '/icon-192x192.png',
+        other: [
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+                url: '/splashscreens/iphone5_splash.png',
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+                url: '/splashscreens/iphone6_splash.png',
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)',
+                url: '/splashscreens/iphoneplus_splash.png',
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+                url: '/splashscreens/iphonex_splash.png',
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+                url: '/splashscreens/iphonexr_splash.png',
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)',
+                url: '/splashscreens/iphonexsmax_splash.png',
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)',
+                url: '/splashscreens/ipad_splash.png',
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)',
+                url: '/splashscreens/ipadpro1_splash.png',
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)',
+                url: '/splashscreens/ipadpro3_splash.png',
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)',
+                url: '/splashscreens/ipadpro2_splash.png',
+            }
+          ],
+        },
     manifest: '/manifest.webmanifest',
 };
